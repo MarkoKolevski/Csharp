8ca14b8adc2047d6ea6cd3bc0477985d3ac5873f
MarkoKolevski <kolevskimarko686@gmail.com>
2025-03-10 13:28:46 +0100

    Add files via upload
    prva domashna

---
+++ CSHARP/domashna1/2/2/2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            string firstInput = Console.ReadLine();
+            int firstNumber = int.Parse(firstInput);
+            Console.WriteLine(firstNumber);
+
+            string secondInput = Console.ReadLine();
+            int secondNumber = int.Parse(secondInput);
+            Console.WriteLine(secondNumber);
+
+            string thirdInput = Console.ReadLine();
+            Console.WriteLine(thirdInput);
+
+
+            if (thirdInput == "+")
+            {
+                Console.WriteLine(firstNumber + secondNumber);
+            }
+            else if (thirdInput == "-")
+            {
+                Console.WriteLine(firstNumber - secondNumber);
+            }
+            else if (thirdInput == "/")
+            {
+                Console.WriteLine(firstNumber / secondNumber);
+            }
+            else if (thirdInput == "*")
+            {
+                Console.WriteLine(firstNumber * secondNumber);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CSHARP/domashna1/Domashna1/Domashna1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Domashna1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstInput  = Console.ReadLine();
+            double firstNumber = double.Parse(firstInput);
+            Console.WriteLine("first number :" + firstNumber);
+
+            string secondInput = Console.ReadLine();
+            double secondNumber = double.Parse(secondInput);
+            Console.WriteLine("second number:" + secondNumber);
+
+            string thirdInput = Console.ReadLine();
+            double thirdNumber = double.Parse(thirdInput);
+            Console.WriteLine("thrid number:" + thirdNumber);
+
+            double allNumbers = firstNumber + secondNumber + thirdNumber;
+
+            Console.WriteLine("average number is :" + allNumbers / 3);
+            Console.ReadLine();
+
+        }
+    }
+}
